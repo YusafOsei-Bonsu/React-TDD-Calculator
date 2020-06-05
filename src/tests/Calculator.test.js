c777644bad70d4da485e4e27df0f53180d389d5e
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-05 11:36:59 +0100

    Test checks if the Display comp exists in the Calc comp

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from '../containers/Calculator';
+import Display from '../containers/Display';
 
 describe('Calculator Component', () => {
     let wrapper;
@@ -9,4 +10,7 @@ describe('Calculator Component', () => {
 
     // Checks if the component renders as a div
     test('should render a <div />', () => expect(wrapper.find('div').length).toEqual(1));
+
+    // Checks if the Display component is within the Calculator comp
+    test('should render the Display component', () => expect(wrapper.containsMatchingElement(<Display />)).toEqual(true));
 });
