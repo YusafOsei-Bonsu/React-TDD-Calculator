5f96a9bccdde13bbd4f5e86ac34d77093dce8e79
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-05 09:32:15 +0100

    Additional test case
    Checking if the calculator component exists inside the App component.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../containers/App.js';
+import App from '../containers/App';
+import Calculator from '../containers/Calculator'
 
 describe('App', () => {
-  test('should render a <div />', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div').length).toEqual(1);
-  })
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
+  // Checks if the App component exists
+  test('should render a <div />', () => expect(wrapper.find('div').length).toEqual(1));
+
+  // Checks if the Calculator component exists inside the App component
+  test('should render the Calculator component', () => expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true));
   
-})
+});
