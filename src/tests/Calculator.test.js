762566636a379fba5d3dcf93c71a900a35e2fffb
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-06 16:22:09 +0100

    Mounted calc tests

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Display from '../containers/Display.jsx';
 import Calculator from '../containers/Calculator.jsx';
 import Keypad from '../containers/Keypad.jsx'
@@ -34,4 +34,38 @@ describe('Calculator Component', () => {
         ])).toEqual(true);
     });
      
+});
+
+describe('Mounted Calculator', () => {
+    let wrapper;
+    
+    beforeEach(() => wrapper = mount(<Calculator />));
+
+    // Testing the invocation of 'updateDisplay' when a number key is clicked
+    test('should call updateDisplay when a numeric key is clicked', () => {
+        const spy = jest.spyOn(wrapper.instance(), 'updateDisplay');
+        wrapper.instance().forceUpdate();
+        expect(spy).toHaveBeenCalledTimes(0);
+        wrapper.find('.number-key').first().simulate('click');
+        expect(spy).toHaveBeenCalledTimes(1); 
+    });
+
+    // Testing the invocation of 'setOperator' when an operator is clicked
+    test('should call setOperator when an operator is clicked', () => {
+        const spy = jest.spyOn(wrapper.instance(), 'setOperator');
+        wrapper.instance().forceUpdate();
+        expect(spy).toHaveBeenCalledTimes(0);
+        wrapper.find('.operator-key').first().simulate('click');
+        expect(spy).toHaveBeenCalledTimes(1);     
+    });
+
+    // Testing the invocation of 'callOperator' when the submit btn is clicked
+    test('calls callOperator when the submit key is clicked', () => {
+        const spy = jest.spyOn(wrapper.instance(), 'callOperator');
+        wrapper.instance().forceUpdate();
+        expect(spy).toHaveBeenCalledTimes(0);
+        wrapper.find('.submit-key').simulate('click');
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
 });
